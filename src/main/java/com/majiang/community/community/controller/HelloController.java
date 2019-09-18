98e2ee3987f19a3280171db98078920456b1ec10
yamon <1165828007@qq.com>
2019-09-18 20:09:03 +0800

    quickSpringboot

---
+++ src/main/java/com/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Intellij IDEA.
+ * User:  陈亚萌
+ * Date:  2019/9/18
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name",required=false, defaultValue="World") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
