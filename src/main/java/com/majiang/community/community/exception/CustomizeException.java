9a3b85fb5c473bfd12753f772e48e6e8e88e4e8d
yamon <lovechen1996428@qq.com>
2020-02-10 22:29:17 +0800

    提交

--- src/main/java/com/majiang/community/community/exception/CustomizeException.java
+++ src/main/java/com/majiang/community/community/exception/CustomizeException.java
@@ -15,7 +15,6 @@ public class CustomizeException extends RuntimeException  {
     public CustomizeException(ICustomizeErrorCode errorCode) {
         this.code=errorCode.getCode();
         this.message= errorCode.getMessage();
-
     }
 
     @Override
